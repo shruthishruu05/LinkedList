df9d0d258755bb92368190653ce05b4a560f273d
shruthishruu05 <shruthi.m@perfios.com>
2021-08-31 21:25:54 +0530

    MODIFIED: Search method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -3,19 +3,15 @@ package com.bridgelabz.linkedlist;
 public class LinkedListMain {
 	public static void main(String[] args)
 	{
-		
-		
-		System.out.println("Demonstration of  nodes insertion to linked list");
+		System.out.println("Demonstration of  Search in linked list");
 		MyNode<Integer> node1 = new MyNode<Integer>(70);
 		MyNode<Integer> node2 = new MyNode<Integer>(56);
 		MyNode<Integer> node3 = new MyNode<Integer>(30);
 		LinkedList myLinkedListNode = new LinkedList();
-		myLinkedListNode.appendNodes(node1);
-		myLinkedListNode.appendNodes(node2);
-		myLinkedListNode.insert(node3);
-		Object deleteNode = myLinkedListNode.popLast();
-		System.out.println("the deleted value is "+deleteNode);
-		
+		myLinkedListNode.add(node1);
+		myLinkedListNode.add(node2);
+		myLinkedListNode.add(node3);
+		myLinkedListNode.search(node3);
 		myLinkedListNode.printLinkedList();
 		
 		
