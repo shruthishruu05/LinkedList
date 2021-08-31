e50246dd4e64c5c1a65d51ff605cee44703b5b72
shruthishruu05 <shruthi.m@perfios.com>
2021-08-31 19:18:01 +0530

    REFACTORED: used insert method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -3,25 +3,19 @@ package com.bridgelabz.linkedlist;
 public class LinkedListMain {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to linked list in data structure");
-		MyNode<Integer> firstNode = new MyNode<Integer>(70);
-		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
-		LinkedList myLinkedList = new LinkedList();
-		myLinkedList.add(firstNode);
-		myLinkedList.add(secondNode);
-		myLinkedList.add(thirdNode);
-		myLinkedList.printLinkedList();
 		
-		System.out.println("appending nodes to linked list");
+		
+		System.out.println("Demonstration of  nodes insertion to linked list");
 		MyNode<Integer> node1 = new MyNode<Integer>(70);
-		MyNode<Integer> node2 = new MyNode<Integer>(30);
-		MyNode<Integer> node3 = new MyNode<Integer>(56);
-		LinkedList myLinkedListNode1 = new LinkedList();
-		myLinkedListNode1.appendNodes(firstNode);
-		myLinkedListNode1.appendNodes(secondNode);
-		myLinkedListNode1.appendNodes(thirdNode);
-		myLinkedListNode1.printLinkedList();
+		MyNode<Integer> node2 = new MyNode<Integer>(56);
+		MyNode<Integer> node3 = new MyNode<Integer>(30);
+		LinkedList myLinkedListNode = new LinkedList();
+		myLinkedListNode.appendNodes(node1);
+		myLinkedListNode.appendNodes(node2);
+		myLinkedListNode.insert(node3);
+		myLinkedListNode.printLinkedList();
+		
+		
 	}
 
 }
