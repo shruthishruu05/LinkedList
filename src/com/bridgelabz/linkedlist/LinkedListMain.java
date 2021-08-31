8e8e32471b3160fc2d97a10db02f5805ca7daf00
shruthishruu05 <shruthi.m@perfios.com>
2021-08-31 17:49:18 +0530

    MODIFIED: main method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,11 +4,16 @@ public class LinkedListMain {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to linked list in data structure");
-		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
-		firstNode.setNext(secondNode);
-		secondNode.setNext(thirdNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		LinkedList myLinkedList = new LinkedList();
+		myLinkedList.add(firstNode);
+		myLinkedList.add(secondNode);
+		myLinkedList.add(thirdNode);
+		myLinkedList.printLinkedList();
+		
+		
 	}
 
 }
