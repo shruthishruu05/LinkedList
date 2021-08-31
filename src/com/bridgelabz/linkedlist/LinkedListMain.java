b7349645bfe2d67cf22a0386a26478b8f347439d
shruthishruu05 <shruthi.m@perfios.com>
2021-08-31 18:41:52 +0530

    REFACTORED : Append method added

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -6,7 +6,22 @@ public class LinkedListMain {
 		System.out.println("Welcome to linked list in data structure");
 		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(56);	
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		LinkedList myLinkedList = new LinkedList();
+		myLinkedList.add(firstNode);
+		myLinkedList.add(secondNode);
+		myLinkedList.add(thirdNode);
+		myLinkedList.printLinkedList();
+		
+		System.out.println("appending node to linked list");
+		MyNode<Integer> node1 = new MyNode<Integer>(70);
+		MyNode<Integer> node2 = new MyNode<Integer>(30);
+		MyNode<Integer> node3 = new MyNode<Integer>(56);
+		LinkedList myLinkedListNode1 = new LinkedList();
+		myLinkedListNode1.appendNodes(firstNode);
+		myLinkedListNode1.appendNodes(secondNode);
+		myLinkedListNode1.appendNodes(thirdNode);
+		myLinkedListNode1.printLinkedList();
 	}
 
 }
