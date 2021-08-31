d92e00ad49105aa9af66c37f5bce7da6c6ece133
shruthishruu05 <shruthi.m@perfios.com>
2021-08-31 17:52:40 +0530

    REFACTORED : implements INode interface

--- src/com/bridgelabz/linkedlist/MyNode.java
+++ src/com/bridgelabz/linkedlist/MyNode.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.linkedlist;
 
-public class MyNode<K> 
+public class MyNode<K> implements INode<K>
 {
 	private K key;
-	private MyNode next;
+	private INode<K> next;
 	public MyNode(K key) 
 	{
 		super();
@@ -11,15 +11,40 @@ public class MyNode<K>
 		this.next = null;
 	}
 	
+	@Override
+	public K getKey()
+	{
+		return key;
+	}
+	@Override
+	public void setKey(K key)
+	{
+		this.key = key;
+	}
 	
-	public MyNode getNext()
+	@Override
+	public String toString() {
+		StringBuilder myNodeString = new StringBuilder();
+		myNodeString.append ("{"+"Key=").append(key).append('}');
+		if(next!=null)
+		{
+			myNodeString.append("-->").append(next);
+		}
+		return myNodeString.toString();
+	}
+
+
+	@Override
+	public INode getNext()
 	{
 		return next;
 	}
 
-
-	public void setNext(MyNode next)
+	@Override
+	public void setNext(INode next)
 	{
 		this.next = next;
 	}
+
+	
 }
